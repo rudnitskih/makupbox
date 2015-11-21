c4be10b8fcd137648f6284eb9e5eded60878c656
Dmytro Rudnytskykh <rudnitskih@gmail.com>
2015-11-21 14:34:35 +0200

    tedd

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,8 @@ gulp generate
 cp -r ./out ./out_min 
 gulp deploy
 rm -r -f ./.publish
-git add .
-testMSG=${1:="auto-commit"}
-git commit -m "testMSG"
+git add . -A
+git commit -m "$1"
 git push bitbucket
 rm -r ./out
 rm -r ./out_min
