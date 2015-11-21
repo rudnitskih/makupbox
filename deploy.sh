08f9c612183ed20b617794c8b6f30f2cbf146c69
Dmytro Rudnytskykh <rudnitskih@gmail.com>
2015-11-21 14:19:44 +0200

    test-fd

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,9 @@
 gulp generate
 cp -r ./out ./out_min 
 gulp deploy
-# rm -r -f ./.publish
-# git add .
-# git commit -m "$1"
-# git push bitbucket
-# rm -r ./out
-# rm -r ./out_min
+rm -r -f ./.publish
+git add .
+git commit -m "$1"
+git push bitbucket
+rm -r ./out
+rm -r ./out_min
